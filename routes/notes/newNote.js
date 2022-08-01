424b73cb696af1e3f9594f4ca7df0cc58c28c360
omar2004712 <o.bahutar@gmail.com>
2022-08-02 00:46:59 +0400

    feat(validators): built a new note validators

--- routes/notes/newNote.js
+++ routes/notes/newNote.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { requireAuth } = require('../middlewares');
 const newNoteTemplate = require('../../views/note/newNoteTemplate');
+const { requireTitle, requireContent } = require('./validators');
 
 const User = mongoose.model('user');
 const router = express.Router();
@@ -10,16 +11,21 @@ router.get('/new-note', requireAuth, (req, res) => {
     res.send(newNoteTemplate({}));
 });
 
-router.post('/api/save-note', requireAuth, async (req, res) => {
-    await User.findByIdAndUpdate(req.session.userId, {
-        $push: {
-            notes: {
-                $each: [req.body],
-                $position: 0,
+router.post(
+    '/api/save-note',
+    requireAuth,
+    [requireTitle, requireContent],
+    async (req, res) => {
+        await User.findByIdAndUpdate(req.session.userId, {
+            $push: {
+                notes: {
+                    $each: [req.body],
+                    $position: 0,
+                },
             },
-        },
-    });
-    res.status(204).send();
-});
+        });
+        res.status(204).send();
+    }
+);
 
 module.exports = router;
---
+++ routes/notes/validators.js
@@ -0,0 +1,13 @@
+const { check } = require('express-validator');
+
+module.exports = {
+    requireTitle: check('title')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Title is required'),
+
+    requireContent: check('content')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Content is require'),
+};
