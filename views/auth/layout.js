9178c93c60b4a2adefdb6e7bee80ba2f800221af
omar2004712 <o.bahutar@gmail.com>
2022-08-06 23:05:05 +0400

    feat: changed the styling of the registration page

--- views/auth/layout.js
+++ views/auth/layout.js
@@ -3,15 +3,22 @@ module.exports = ({ content, title }) => {
   <!DOCTYPE html>
     <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Edu+NSW+ACT+Foundation:wght@400;500;600&family=Edu+QLD+Beginner:wght@400;500;600&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+            <title>${title || 'Notely'}</title>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="images/webIcon.png" />
             <link rel="stylesheet" href="main.css" />
-            <link rel="icon" href="images/webIcon.png"/>
-            <title>${title}</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet"
+            />
+            <script
+            src="https://kit.fontawesome.com/7ecb8aa263.js"
+            crossorigin="anonymous"
+            ></script>
         </head>
         <body>
             ${content}
