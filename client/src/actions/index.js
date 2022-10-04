6a099038ae790edaf6181aa31bd9ae1072bbc1c7
omar2004712 <o.bahutair@gmail.com>
2022-10-04 09:28:38 +0400

    fix: fixed the login button bug
    when the user is not authintecated the server return an empty string which did not have an _id prop
    so the data in the respones if first checked if it is an empty string or not

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,14 @@ import axios from 'axios';
 export const getAuth = () => async (dispatch) => {
   const { data } = await axios.get('/api/current_user');
 
+  if (!data) {
+    // user is not authintecated
+    return dispatch({
+      type: 'AUTH',
+      payload: false,
+    });
+  }
+
   dispatch({
     type: 'AUTH',
     payload: data._id,
