1a405039efc3a80ccb2414ececad86bc8b9f3575
omar2004712 <o.bahutair@gmail.com>
2022-10-05 09:46:51 +0400

    build: built the note form component

--- client/src/components/NoteEditor.jsx
+++ client/src/components/NoteEditor.jsx
@@ -1,10 +1,39 @@
 import '../styles/index.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import NoteForm from './NoteForm';
 
-export default function NoteEditor() {
+function NoteEditor({ note }) {
+  const [noteTitle, setNoteTitle] = useState(note ? note.title : '');
+  const [noteContent, setNoteContent] = useState(note ? note.content : '');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const renderTemplate = () => {
+    return (
+      <NoteForm
+        note={{
+          noteTitle,
+          setNoteTitle,
+          noteContent,
+          setNoteContent,
+          onSubmit,
+        }}
+      />
+    );
+  };
+
   return (
-    <h1 className="flex w-[100vw] h-[100vh] items-center justify-center">
-      Note Editor
-    </h1>
+    <div className="flex w-[100vw] h-[100vh] items-center justify-center">
+      {renderTemplate()}
+    </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  return { note: state.editNote };
+};
+
+export default connect(mapStateToProps)(NoteEditor);
---
+++ client/src/components/NoteForm.jsx
@@ -0,0 +1,37 @@
+import '../styles/index.css';
+import React from 'react';
+
+export default function NoteForm({
+  note: { noteTitle, setNoteTitle, noteContent, setNoteContent, onSubmit },
+}) {
+  const labelClass = 'px-1 font-semibold text-gray-600';
+
+  return (
+    <form
+      className="flex flex-col h-[calc(100%-64px)] w-[100%] self-end p-2 gap-1"
+      onSubmit={onSubmit}
+    >
+      <label className={labelClass}>Note Title</label>
+      <input
+        className="p-2 text-2xl outline-none bg-blue-50 rounded border-2 border-blue-50 focus:border-blue-300"
+        type="text"
+        placeholder="Note Title"
+        value={noteTitle}
+        onChange={(e) => setNoteTitle(e.value)}
+      />
+      <label className={labelClass}>Note Content</label>
+      <textarea
+        className="flex-1 p-2 text-xl outline-none text-gray-500 bg-blue-50 rounded border-2 border-blue-50 focus:border-blue-300 resize-none"
+        placeholder="Note Content"
+        value={noteContent}
+        onChange={(e) => setNoteContent(e.value)}
+      />
+      <button
+        className="bg-yellow-400 w-fit self-end px-3 py-[2px] rounded text-xl"
+        type="submit"
+      >
+        save
+      </button>
+    </form>
+  );
+}
