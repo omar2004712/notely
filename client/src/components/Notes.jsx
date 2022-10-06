43eefe9666ef68c17503579292f13dd8188fcc2f
omar2004712 <o.bahutair@gmail.com>
2022-10-06 20:40:51 +0400

    fix: made the page number to be a state

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -3,39 +3,38 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Note from './Note';
 
-let pageNumber = 0;
+// let pageNumber = 0;
 
 function Notes() {
   const [fetchedNotes, setFetchedNotes] = useState([]);
   const [notes, setNotes] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
 
   const renderNotes = () => {
     return notes.map((note) => <Note key={note._id} note={note} />);
   };
 
-  useEffect(() => {
-    pageNumber = 0;
-    window.addEventListener('scroll', async () => {
-      const NAV_BAR_HEIGHT = 64; // the height of the nav bar 64px
-      if (
-        document.body.scrollHeight - window.scrollY !==
-        window.innerHeight - NAV_BAR_HEIGHT
-      ) {
-        return;
-      }
+  const fetchNotes = async (pageNo) => {
+    const { data } = await axios.post('/api/notes', { index: pageNo });
+    setFetchedNotes(data.notes);
+    setPageNumber(pageNo + 1);
+  };
 
-      const { data } = await axios.post('/api/notes', { index: pageNumber });
-      setFetchedNotes(data.notes);
-      pageNumber++;
-    });
-  }, []);
+  const onScroll = async ({ target }) => {
+    if (target.scrollHeight - target.scrollTop !== target.clientHeight) {
+      return;
+    }
+
+    await fetchNotes(pageNumber);
+  };
 
   useEffect(() => {
     setNotes((notes) => [...notes, ...fetchedNotes]);
   }, [fetchedNotes]);
 
   useEffect(() => {
-    window.dispatchEvent(new Event('scroll'));
+    const INITIAL_PAGE_NUMBER = 0;
+    fetchNotes(INITIAL_PAGE_NUMBER);
   }, []);
 
   useEffect(() => {
@@ -49,7 +48,10 @@ function Notes() {
   }, [notes]);
 
   return (
-    <div className="grid gap-x-2 grid-cols-notes justify-center auto-rows-[1px] mt-[64px] p-4">
+    <div
+      onScroll={onScroll}
+      className="overflow-auto h-[calc(100vh-64px)] grid gap-x-2 grid-cols-notes justify-center auto-rows-[1px] mt-[64px] p-4"
+    >
       {renderNotes()}
     </div>
   );
